fe50662f6eb818b2ad9f033005e8e4774d8c7622
Shellishack <40737228+Shellishack@users.noreply.github.com>
2026-01-18 01:25:48 +0800

    Add icons in service worker

--- web/postbuild.ts
+++ web/postbuild.ts
@@ -9,10 +9,13 @@ const buildDir = path.resolve("../build/next");
 generateSW({
   swDest: `${buildDir}/service-worker.js`,
   globDirectory: buildDir,
-  globPatterns: ["**/*.{html,js,css,png,jpg,svg,json}"],
+  globPatterns: ["**/*.{js,css,png,jpg,svg,json,woff2,woff}"],
+  globIgnores: ["**/service-worker.js"],
   clientsClaim: true,
   skipWaiting: true,
   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
+  navigateFallback: "/",
+  additionalManifestEntries: [{ url: "/", revision: null }],
 })
   .then(({ count, size }) => {
     console.log(`Service worker generated. ${count} files, ${size} bytes.`);
