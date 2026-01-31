b01a8c583df49dfabfc4523d1f880bb735c94a77
Shellishack <40737228+Shellishack@users.noreply.github.com>
2026-01-31 22:39:28 +0800

    Remove unnecessary files

--- web/app/(extension-layout)/extension/layout.tsx
+++ web/app/(extension-layout)/extension/layout.tsx
@@ -1,24 +1,18 @@
-import I18nProvider from "@/components/providers/i18n-provider";
 import RemoteModuleProvider from "@/components/providers/remote-module-provider";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "next-themes";
 import { ReactNode, Suspense } from "react";
-import messages from "../../../messages/en.json";
-
-const locale = "en";
 
 export default function ExtensionLayout({ children }: { children: ReactNode }) {
   return (
     <html lang={locale} suppressHydrationWarning className="h-full w-full">
       <body className="h-full w-full overflow-hidden">
         <Analytics />
-        <I18nProvider locale={locale} messages={messages}>
           <ThemeProvider attribute={["class", "data-theme"]}>
             <RemoteModuleProvider isPreventingCSS={false}>
               <Suspense>{children}</Suspense>
             </RemoteModuleProvider>
           </ThemeProvider>
-        </I18nProvider>
       </body>
     </html>
   );
