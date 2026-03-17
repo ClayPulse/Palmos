3c63c70a6250f0c4361b5a0d6032e2d1dcc3ac9b
Shellishack <40737228+Shellishack@users.noreply.github.com>
2026-03-17 16:10:26 +0800

    Revert "Ignore workflow= search param"
    This reverts commit 10b0d134bf330b3dbb7e16101741559949de8825.

--- web/postbuild.ts
+++ web/postbuild.ts
@@ -30,13 +30,6 @@ generateSW({
   ],
   runtimeCaching: [
     {
-      urlPattern: ({ url }) => url.search.includes("workflow="),
-      handler: "NetworkOnly",
-      options: {
-        cacheName: "workflow-routes",
-      },
-    },
-    {
       urlPattern: ({ request }) => request.mode === "navigate",
       handler: "NetworkFirst",
       options: {
